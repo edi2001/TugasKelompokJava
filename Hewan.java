58c14bc5807bcf218f07bb28b99a566206c903b3
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2022-08-30 16:03:18 +0700

    Minor changes

--- Hewan.java
+++ Hewan.java
@@ -1,11 +1,11 @@
 public class Hewan extends Makhluk {
 
   void bernafas() {
-    System.out.println("Ya, bernafas kalo masih hidup");
+    System.out.println("Bernafas kalo hidup");
   }
 
   void bergerak() {
-    System.out.println("Ya, bisa bergerak kalo masih hidup");
+    System.out.println("Bergerak kalo masih hidup");
   }
 
   void suara() {
